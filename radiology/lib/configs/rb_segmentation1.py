322bc07f1693767efee86ac8fc6d6b70007432c1
Obodo David <obododavid5@gmail.com>
2025-05-23 13:07:21 +0100

    feat: modify model1 config file to match with segementation config file instead of segmentation_spleen config file

--- radiology/lib/configs/rb_segmentation1.py
+++ radiology/lib/configs/rb_segmentation1.py
@@ -15,37 +15,34 @@ from typing import Any, Dict, Optional, Union
 
 import lib.infers
 import lib.trainers
-from monai.networks.nets import UNet
+from monai.networks.nets import SegResNet
+from monai.utils import optional_import
 
 from monailabel.interfaces.config import TaskConfig
 from monailabel.interfaces.tasks.infer_v2 import InferTask
-from monailabel.interfaces.tasks.scoring import ScoringMethod
-from monailabel.interfaces.tasks.strategy import Strategy
 from monailabel.interfaces.tasks.train import TrainTask
-from monailabel.tasks.activelearning.epistemic import Epistemic
-from monailabel.tasks.scoring.dice import Dice
-from monailabel.tasks.scoring.epistemic import EpistemicScoring
-from monailabel.tasks.scoring.sum import Sum
-from monailabel.utils.others.generic import download_file, strtobool
+from monailabel.utils.others.generic import download_file, remove_file, strtobool
+
+_, has_cp = optional_import("cupy")
+_, has_cucim = optional_import("cucim")
 
 logger = logging.getLogger(__name__)
 
 
 class RBSegmentation1(TaskConfig):
-    def __init__(self):
-        super().__init__()
-
-        self.epistemic_enabled = None
-        self.epistemic_samples = None
-
     def init(self, name: str, model_dir: str, conf: Dict[str, str], planner: Any, **kwargs):
         super().init(name, model_dir, conf, planner, **kwargs)
 
         # Labels
-        self.labels = {
-            "left kidney": 1,
-            "right kidney": 2
-        }
+        conf_labels = self.conf.get("labels")
+        self.labels = (
+            {label: idx for idx, label in enumerate(conf_labels.split(","), start=1)}
+            if conf_labels
+            else {
+                "left kidney": 1,
+                "right kidney": 2,
+            }
+        )
 
         # Model Files
         self.path = [
@@ -54,30 +51,29 @@ class RBSegmentation1(TaskConfig):
         ]
 
         # Download PreTrained Model
-        if strtobool(self.conf.get("use_pretrained_model", "false")):
+        if not conf_labels and strtobool(self.conf.get("use_pretrained_model", "false")):
             url = f"{self.conf.get('pretrained_path', self.PRE_TRAINED_PATH)}"
-            url = f"{url}/radiology_segmentation_unet_spleen_total_seg.pt"
+            url = f"{url}/radiology_segmentation_segresnet_multilabel.pt"
             download_file(url, self.path[0])
 
-        self.target_spacing = (1.0, 1.0, 1.0)  # target space for image
+        # Remove pre-trained pt if user is using his/her custom labels.
+        if conf_labels:
+            remove_file(self.path[0])
+
+        self.target_spacing = (1.5, 1.5, 1.5)  # target space for image
         # Setting ROI size - This is for the image padding
         self.roi_size = (96, 96, 96)
 
         # Network
-        self.network = UNet(
+        self.network = SegResNet(
             spatial_dims=3,
             in_channels=1,
-            out_channels=len(self.labels) + 1,  # labels plus background
-            channels=[16, 32, 64, 128, 256],
-            strides=[2, 2, 2, 2],
-            num_res_units=2,
-            norm="batch",
+            out_channels=len(self.labels) + 1,  # labels plus background,
+            init_filters=32,
+            blocks_down=(1, 2, 2, 4),
+            blocks_up=(1, 1, 1),
+            dropout_prob=0.2,
         )
-
-        # Others
-        self.epistemic_enabled = strtobool(conf.get("epistemic_enabled", "false"))
-        self.epistemic_samples = int(conf.get("epistemic_samples", "5"))
-        logger.info(f"EPISTEMIC Enabled: {self.epistemic_enabled}; Samples: {self.epistemic_samples}")
 
     def infer(self) -> Union[InferTask, Dict[str, InferTask]]:
         task: InferTask = lib.infers.RBSegmentation1(
@@ -87,6 +83,7 @@ class RBSegmentation1(TaskConfig):
             target_spacing=self.target_spacing,
             labels=self.labels,
             preload=strtobool(self.conf.get("preload", "false")),
+            config={"largest_cc": True if has_cp and has_cucim else False},
         )
         return task
 
@@ -99,40 +96,9 @@ class RBSegmentation1(TaskConfig):
             network=self.network,
             roi_size=self.roi_size,
             target_spacing=self.target_spacing,
-            description="Train Redbrick first Segmentation Model",
             load_path=load_path,
             publish_path=self.path[1],
+            description="Train Redbrick first Segmentation Model",
             labels=self.labels,
-            disable_meta_tracking=False,
         )
         return task
-
-    def strategy(self) -> Union[None, Strategy, Dict[str, Strategy]]:
-        strategies: Dict[str, Strategy] = {}
-        if self.epistemic_enabled:
-            strategies[f"{self.name}_epistemic"] = Epistemic()
-        return strategies
-
-    def scoring_method(self) -> Union[None, ScoringMethod, Dict[str, ScoringMethod]]:
-        methods: Dict[str, ScoringMethod] = {
-            "dice": Dice(),
-            "sum": Sum(),
-        }
-
-        if self.epistemic_enabled:
-            methods[f"{self.name}_epistemic"] = EpistemicScoring(
-                model=self.path,
-                network=UNet(
-                    spatial_dims=3,
-                    in_channels=1,
-                    out_channels=2,
-                    channels=[16, 32, 64, 128, 256],
-                    strides=[2, 2, 2, 2],
-                    num_res_units=2,
-                    norm="batch",
-                    dropout=0.2,
-                ),
-                transforms=lib.infers.RBSegmentation1(None).pre_transforms(),
-                num_samples=self.epistemic_samples,
-            )
-        return methods
